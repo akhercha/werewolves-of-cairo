55c0a90c7e283d6e216553fcf3d1075dc779f226
akhercha <akherchache@pm.me>
2023-11-08 21:41:06 +0100

    main: quick update

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -4,21 +4,37 @@ pushd $(dirname "$0")/..
 
 export RPC_URL="http://localhost:5050";
 
-export WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
+export WORLD_ADDRESS=$(cat ./contracts/target/dev/manifest.json | jq -r '.world.address')
+export GAME_ADDRESS=$(cat ./contracts/target/dev/manifest.json | jq -r '.contracts[] | select(.name == "game" ).address')
+export LOBBY_ADDRESS=$(cat ./contracts/target/dev/manifest.json | jq -r '.contracts[] | select(.name == "lobby" ).address')
+export PLAYER_ADDRESS=$(cat ./contracts/target/dev/manifest.json | jq -r '.contracts[] | select(.name == "player" ).address')
+export PROFILE_ADDRESS=$(cat ./contracts/target/dev/manifest.json | jq -r '.contracts[] | select(.name == "profile" ).address')
+export WAITER_ADDRESS=$(cat ./contracts/target/dev/manifest.json | jq -r '.contracts[] | select(.name == "waiter" ).address')
 
-export ACTIONS_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "actions" ).address')
 
 echo "---------------------------------------------------------------------------"
+echo rpc  : $RPC_URL 
+echo " "
 echo world : $WORLD_ADDRESS 
 echo " "
-echo actions : $ACTIONS_ADDRESS
+echo game : $GAME_ADDRESS
+echo lobby : $LOBBY_ADDRESS
+echo player : $PLAYER_ADDRESS
+echo profile : $PROFILE_ADDRESS
+echo waiter : $WAITER_ADDRESS
 echo "---------------------------------------------------------------------------"
 
-# enable system -> component authorizations
-COMPONENTS=("Position" "Moves" )
 
-for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component $ACTIONS_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+GAME_MODELS=("Game" "Market" "Player" )
+LOBBY_MODELS=("Player" "Market" "Encounter")
+PLAYER_MODELS=("Player" "Drug" "Market" "Encounter")
+PROFILE_MODELS=("Drug" "Market" "Player")
+WAITER_MODELS=("Player" "Item" "Market")
+
+for model in ${LOBBY_MODELS[@]}; do
+    sozo auth writer $model $LOBBY_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sleep 0.1
 done
+
 
 echo "Default authorizations have been successfully set."
